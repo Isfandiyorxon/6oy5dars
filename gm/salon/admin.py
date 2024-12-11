31f9bc22ac47007f40a3c9a3ccf96b08476b0e99
Isfandiyor <foziljonovisfandiyor@gmail.com>
2024-12-11 16:24:19 +0500

    admin panel yakunlandi

--- gm/salon/admin.py
+++ gm/salon/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
 from .models import Cars, Brend
-
-admin.site.register(Cars)
+from django.utils.safestring import mark_safe
 admin.site.register(Brend)
+class Carsadmin(admin.ModelAdmin):
+    list_display = ('pk','model','brend','color','max_speed','probeg','sotuvda_bormi','get_poto')
+    list_display_links = ('model','pk')
+    list_editable = ('sotuvda_bormi', 'brend')
+    list_filter = ('brend','sotuvda_bormi')
+    search_fields = ('pk','model')
+    list_per_page = 5
+
+
+    def get_poto(self,car):
+        if car.photo:
+            return mark_safe(f'<img src="{car.photo.url}" width="150px" />')
+        else:
+            return '-'
+
+    get_poto.short_description="Rasmi"
+
+
+
+admin.site.register(Cars, Carsadmin)
 
 # Register your models here.
--- gm/salon/models.py
+++ gm/salon/models.py
@@ -3,23 +3,34 @@ from django.db import models
 # Create your models here.
 
 class Brend(models.Model):
-    brend_name=models.CharField(max_length=150,unique=True)
+    brend_name=models.CharField(max_length=150,unique=True,verbose_name='Kategoriya nomi')
+
+
+    class Meta:
+        verbose_name="Brend"
+        verbose_name_plural='Brendlar'
 
     def __str__(self):
         return self.brend_name
 
 class Cars(models.Model):
-    brend=models.ForeignKey(Brend,on_delete=models.CASCADE)
-    model=models.CharField(max_length=100)
-    color=models.CharField(max_length=7,default='#FFFFFF')
-    photo=models.ImageField(upload_to='cars/',blank=True,null=True)
-    probeg=models.IntegerField(default=0)
-    max_speed=models.IntegerField(default=160)
-    price=models.DecimalField(max_digits=12,decimal_places=2)
-    sotuvda_bormi=models.BooleanField(default=True)
+    brend=models.ForeignKey(Brend,on_delete=models.CASCADE,verbose_name='mashina brendi')
+    model=models.CharField(max_length=100,verbose_name='mashina modeli')
+    color=models.CharField(max_length=7,default='#FFFFFF',verbose_name='mashina rangi')
+    photo=models.ImageField(upload_to='cars/',blank=True,null=True,verbose_name='mashina rasmi')
+    probeg=models.IntegerField(default=0,verbose_name="mashina bosib o'tgan yo'li")
+    max_speed=models.IntegerField(default=160,verbose_name='maximal tezligi')
+    price=models.DecimalField(max_digits=12,decimal_places=2,verbose_name='mashina narxi')
+    sotuvda_bormi=models.BooleanField(default=True,verbose_name='mashina sotuvda mavjud',
+                                      help_text="Agar galochka qo'yilgan bo'lsa sotuvda bo bo'ladi")
+
     def __str__(self):
         return self.model
 
 
+    class Meta:
+        verbose_name="Mashina"
+        verbose_name_plural='Mashinalar'
+        ordering=['pk']
 
 
